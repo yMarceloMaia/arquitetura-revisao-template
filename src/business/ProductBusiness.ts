de0db7f5364ad26395bc20e4a56c9542f1a0cb40
Marcelo Maia Lima <marcelomaiajp@gmail.com>
2023-04-27 14:14:02 -0300

    update

--- src/business/ProductBusiness.ts
+++ src/business/ProductBusiness.ts
@@ -9,10 +9,7 @@ export class ProductBusiness{
         let productsDB
 
         if(q){
-            // caso receba uma querie com o nome do produto
-            // aqui vai ter a implementação da comunicação com o banco de dados
-            // preciso chamar o metodo do ProductDatabase
-            // getProductByName
+            
             productsDB = await this.productDatabase.getProductByName(q)
         }else{
             // chamar o metodo de buscar todos os produtos
